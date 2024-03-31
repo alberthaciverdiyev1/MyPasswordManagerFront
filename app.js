e89300200f4f9606c2471462ea3a05388e22fe8c
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-03-31 01:23:21 -0700

    setup project

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+// const expressSession = require('express-session');
+const bodyParser = require('body-parser');
+  //CUSTOM
+const routes = require('./routes/routes.js');
+require('dotenv').config();
+
+const port = 8080;
+const app = express();
+
+app.use(express.static('assets'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
